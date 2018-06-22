63ac6b3adf85995967d807373da0805f1a3f2f61
ahmedneem23 <40441103+ahmedneem23@users.noreply.github.com>
2018-06-22 06:42:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.TOKEN = 'NDU5NTc3NDY5MjgwODQ1ODI3.Dg4Obw.jESFl2rZpRZjNaYR76jrVnoOsJU';
 
-exports.PREFIX = '3';
+exports.PREFIX = '!';
 
 exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
